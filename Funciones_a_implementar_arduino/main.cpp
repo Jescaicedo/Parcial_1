26bee26cf4b4e26110fa8521787499497d201cbd
Juan Caicedo <jesteban.caicedo@udea.edu.co>
2023-09-25 18:00:58 -0500

    Implementacion funcion patron 4

--- Funciones_a_implementar_arduino/main.cpp
+++ Funciones_a_implementar_arduino/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void patron1(int **);
 void patron2(int **);
 void patron3(int **);
+void patron4(int **);
 
 int main()
 {
@@ -18,7 +19,7 @@ int main()
                 matriz[i][j] = 0;
             }
         }
-        patron3(matriz);
+        patron4(matriz);
         for(int i=0; i<8;i++){
             for(int j=0; j<8;j++){
                 cout<<matriz[i][j]<<" ";
@@ -93,6 +94,30 @@ void patron3(int **matriz)
             else if(aux=2){
                 aux=0;
             }
+        }
+    }
+}
+
+void patron4(int **matriz)
+{
+    bool ban=true;
+    int cont=0, lim=0;
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(j>=lim && cont<4){
+                matriz[i][j]=1;
+                cont+=1;
+            }
+        }
+        cont=0;
+        if(i==3){
+            ban=false;
+        }
+        else if(ban){
+            lim+=1;
+        }
+        else if(!ban){
+            lim-=1;
         }
     }
 }
